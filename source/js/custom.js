39b48445f853f7287528be6cb29aef7475ab1234
fanlumaster <1730976608@gmail.com>
2024-08-19 14:00:23 +0800

    make toggle navbar compatible with vimiumc

--- source/js/custom.js
+++ source/js/custom.js
@@ -49,21 +49,19 @@ function listenScroll(callback) {
 
 
 var navbarWidget = document.getElementById("navbar");
-// var prevScrollPos = window.pageYOffset || document.documentElement.scrollTop;
 var prevScrollPos = Number.MAX_VALUE; // initial state
+// 为什么要定义 prev 的 prev 呢？因为这个是为了解决 vimiumc 插件的兼容问题的，使用 vimiumc 的 j 和 k 进行滚动时，很多时候一瞬间会获取两个坐标，这个响应的间隔比 scroll 事件的监听函数执行间隔要短，所以这里就兼容一下
+var prevPrevScrollPos = Number.MAX_VALUE; // initial state
 function helper() {
   var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
-  if (prevScrollPos > currentScrollPos) {
+  if (prevPrevScrollPos >= currentScrollPos) {
     // 向上滚动，显示导航栏
-    // document.querySelector('nav').style.top = '0';
-    // navbarWidget.classList.remove('nav-hidden');
     navbarWidget.style.top = '0';
   } else {
     // 向下滚动，隐藏导航栏
-    // document.querySelector('nav').style.top = '-100px'; // 根据需要设置隐藏的位置
-    // navbarWidget.classList.add('nav-hidden');
     navbarWidget.style.top = '-100px';
   }
+  prevPrevScrollPos = prevScrollPos;
   prevScrollPos = currentScrollPos;
 }
 
@@ -75,7 +73,3 @@ function toggleNavbar() {
 document.addEventListener("DOMContentLoaded", function() {
   toggleNavbar();
 });
-
-// window.onload = function () {
-//   toggleNavbar();
-// };
